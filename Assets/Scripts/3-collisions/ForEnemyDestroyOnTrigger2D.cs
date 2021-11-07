ebfbffd89289c60a98c3ac72b04b360b613a0144
avichai1221 <57321080+avichai1221@users.noreply.github.com>
2021-11-08 01:16:45 +0200

    first

---
+++ Assets/Scripts/3-collisions/ForEnemyDestroyOnTrigger2D.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class ForEnemyDestroyOnTrigger2D : MonoBehaviour {
+
+    
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+  
+    [SerializeField] int maxTypeEnemy;
+
+
+   
+
+ private void OnTriggerEnter2D(Collider2D other) {
+        for(int i=maxTypeEnemy;i>0;i--){
+        if (other.tag==i.ToString()&& enabled) {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+        }
+    }
+
+
+
+    private void Update() {
+        /* Just to show the enabled checkbox in Editor */
+    }
+}
---
+++ Assets/Scripts/3-collisions/ScoreAdder.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/**
+ * This component increases a given "score" field whenever it is triggered.
+ */
+public class ScoreAdder : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger adding score to the score field.")]
+    
+    [SerializeField] NumberField scoreField;
+    [SerializeField] int maxTypeEnemy;
+    
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        for(int i=1;i<=maxTypeEnemy;i++){
+        if (other.tag == i.ToString()) {
+            scoreField.AddNumber(i);
+        }
+        
+        }
+    }
+
+    public void SetScoreField(NumberField newTextField) {
+        this.scoreField = newTextField;
+    }
+}
