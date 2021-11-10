c89a40d95cd32783c83ea8c7f207ceeb53a08cf4
avichai1221 <57321080+avichai1221@users.noreply.github.com>
2021-11-10 03:51:41 +0200

    add text -game over

--- Assets/Scripts/3-collisions/ForEnemyDestroyOnTrigger2D.cs
+++ Assets/Scripts/3-collisions/ForEnemyDestroyOnTrigger2D.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /**
  * This component destroys its object whenever it triggers a 2D collider with the given tag.
@@ -11,6 +12,7 @@ public class ForEnemyDestroyOnTrigger2D : MonoBehaviour {
     [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
   
     [SerializeField] int maxTypeEnemy;
+    [SerializeField] string sceneName;
 
 
  private void OnTriggerEnter2D(Collider2D other) {
@@ -18,7 +20,8 @@ public class ForEnemyDestroyOnTrigger2D : MonoBehaviour {
         if ((other.tag==i.ToString()||other.tag=="points")&& enabled) {
             Destroy(this.gameObject);
             Destroy(other.gameObject);
-            
+             SceneManager.LoadScene(sceneName);
+
         }
         }
         
